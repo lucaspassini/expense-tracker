9d06ffdb8264cd60957a3b57c6f3ddd043062481
Lucas Cristiano Passini <passini@protonmail.com>
2022-02-05 22:16:46 -0300

    add components and state hooks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -8,12 +9,7 @@ const App = () => {
       amount: 94.12,
       date: new Date(2020, 7, 14)
     },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -27,9 +23,22 @@ const App = () => {
       date: new Date(2021, 5, 12)
     }
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
